86edfc4910b18cdd597b1e44b788668841cb0ce9
bradley vangelder <r0784535@student.thomasmore.be>
2021-10-01 11:18:04 +0200

    initial commit

---
+++ src/main/java/fat/it/bookquotes/BookquotesApplication.java
@@ -0,0 +1,13 @@
+package fat.it.bookquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookquotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookquotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fat/it/bookquotes/BookquotesApplicationTests.java
@@ -0,0 +1,13 @@
+package fat.it.bookquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookquotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
