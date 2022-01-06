c1a038d57f6451aaa13ac46b6050c791dd023744
Sam Poelmans <r0790203@student.thomasmore.be>
2022-01-06 12:49:04 +0100

    unit&integration

--- src/test/java/fat/it/bookquotes/QuotesControllerIntegrationTest.java
+++ src/test/java/fat/it/bookquotes/QuotesControllerIntegrationTest.java
@@ -52,11 +52,11 @@ class QuoteControllerIntegrationTests {
     private ObjectMapper mapper = new ObjectMapper();
 
     @Test
-    public void givenReview_whenGetReviewsByISBN_thenReturnJsonReviews() throws Exception {
+    public void givenQuotes_whenGetQuotesByISBN_thenReturnJsonReviews() throws Exception {
 
-        List<Quote> reviewList = new ArrayList<>();
-        reviewList.add(quote1);
-        reviewList.add(quote2);
+        List<Quote> quoteList = new ArrayList<>();
+        quoteList.add(quote1);
+        quoteList.add(quote2);
 
         mockMvc.perform(get("/quote/book/{isbn}", "6874684345634"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -68,68 +68,39 @@ class QuoteControllerIntegrationTests {
                 .andExpect(jsonPath("$[1].quote", is("Sales people are like actors")));
     }
 
+    @Test
+    public void whenPostQuote_thenReturnJsonReview() throws Exception {
+        Quote quoteBook1 = new Quote("nieuwequote", "ISBN1");
+
+        mockMvc.perform(post("/quote")
+                .content(mapper.writeValueAsString(quoteBook1))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isbn", is("ISBN1")))
+                .andExpect(jsonPath("$.quote", is("nieuwequote")));
+    }
+
 //    @Test
-//    public void givenReview_whenGetReviewsByUserId_thenReturnJsonReviews() throws Exception {
+//    public void givenQuote_whenPutQuote_thenReturnJsonReview() throws Exception {
 //
-//        List<Review> reviewList = new ArrayList<>();
-//        reviewList.add(reviewUser1Book1);
-//        reviewList.add(reviewUser1Book2);
+//        Quote updatedQuote = new Quote("nieuwequote", "ISBN1");
 //
-//        mockMvc.perform(get("/reviews/user/{userId}", 001))
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$", hasSize(2)))
-//                .andExpect(jsonPath("$[0].userId", is(001)))
-//                .andExpect(jsonPath("$[0].isbn", is("ISBN1")))
-//                .andExpect(jsonPath("$[0].scoreNumber", is(1)))
-//                .andExpect(jsonPath("$[1].userId", is(001)))
-//                .andExpect(jsonPath("$[1].isbn", is("ISBN2")))
-//                .andExpect(jsonPath("$[1].scoreNumber", is(2)));
-//    }
-//
-//    @Test
-//    public void whenPostReview_thenReturnJsonReview() throws Exception {
-//        Review reviewUser3Book1 = new Review(003, "ISBN1", 1);
-//
-//        mockMvc.perform(post("/reviews")
-//                .content(mapper.writeValueAsString(reviewUser3Book1))
+//        mockMvc.perform(put("/quote/{id}",1)
+//                .content(mapper.writeValueAsString(updatedQuote))
 //                .contentType(MediaType.APPLICATION_JSON))
 //                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 //                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.userId", is(003)))
 //                .andExpect(jsonPath("$.isbn", is("ISBN1")))
-//                .andExpect(jsonPath("$.scoreNumber", is(1)));
+//                .andExpect(jsonPath("$.quote", is("nieuwequote")));
 //    }
-//
-//    @Test
-//    public void givenReview_whenPutReview_thenReturnJsonReview() throws Exception {
-//
-//        Review updatedReview = new Review(001, "ISBN1", 2);
-//
-//        mockMvc.perform(put("/reviews")
-//                .content(mapper.writeValueAsString(updatedReview))
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.userId", is(001)))
-//                .andExpect(jsonPath("$.isbn", is("ISBN1")))
-//                .andExpect(jsonPath("$.scoreNumber", is(2)));
-//    }
-//
-//    @Test
-//    public void givenReview_whenDeleteReview_thenStatusOk() throws Exception {
-//
-//        mockMvc.perform(delete("/reviews/user/{userId}/book/{ISBN}", 999, "ISBN9")
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void givenNoReview_whenDeleteReview_thenStatusNotFound() throws Exception {
-//
-//        mockMvc.perform(delete("/reviews/user/{userId}/book/{ISBN}", 888, "ISBN8")
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isNotFound());
-//    }
+
+    @Test
+    public void givenQuote_whenDeleteQuote_thenStatusOk() throws Exception {
+
+        mockMvc.perform(delete("/quote/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
 }
